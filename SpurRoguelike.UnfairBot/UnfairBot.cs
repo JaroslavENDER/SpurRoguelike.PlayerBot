c7de059cd6bb8d66c7f70c98025810daf437d9e4
Ender <pavlenko.jaroslav@yandex.ru>
2018-03-15 14:16:51 +0700

    Added SpurRoguelike.UnfairBot/UnfairBot.cs

---
+++ SpurRoguelike.UnfairBot/UnfairBot.cs
@@ -0,0 +1,17 @@
+﻿using SpurRoguelike.Core;
+using SpurRoguelike.Core.Primitives;
+using SpurRoguelike.Core.Views;
+using System.Reflection;
+
+namespace SpurRoguelike.UnfairBot
+{
+    public class UnfairBot : IPlayerController
+    {
+        public Turn MakeTurn(LevelView levelView, IMessageReporter messageReporter)
+        {
+            var level = levelView.GetType().GetField("level", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(levelView) as Level;
+            level.Complete();
+            return Turn.None;
+        }
+    }
+}
